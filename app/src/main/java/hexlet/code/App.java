8cfae8211446a3780e4e2ec4776972b2b69da97f
Karelskiy_Andrey <karelskiy.a@gmail.com>
2022-10-18 12:18:35 +0300

    make a help output

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,23 @@
+package hexlet.code;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+//        System.out.println("Hello world!");
+    }
+}
