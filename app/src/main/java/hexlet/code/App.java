b4573788cb9d33d02f51dd403b01540c69ac229f
Karelskiy_Andrey <karelskiy.a@gmail.com>
2022-10-19 14:33:04 +0300

    add arguments and option to cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,6 +9,17 @@ import picocli.CommandLine.Parameters;
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
 
+    @Option(names = { "-f", "--format" }, paramLabel = "format", defaultValue = "stylish",
+            description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @Parameters(index = "0", paramLabel = "filepath1",
+            description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2",
+            description = "path to first file")
+    private String filepath2;
 
     @Override
     public void run() {
