0c89d423b157d113e404e1a56ceb8874b38de157
Karelskiy_Andrey <karelskiy.a@gmail.com>
2022-10-26 10:30:13 +0300

    add nested files

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -10,10 +11,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class DifferTest {
     private final String jsonFilePath1 = "./src/test/resources/file1.json";
     private final String jsonFilePath2 = "./src/test/resources/file2.json";
+    private final String nestedJsonFilePath1 = "./src/test/resources/nestedFile1.json";
+    private final String nestedJsonFilePath2 = "./src/test/resources/nestedFile2.json";
     private final String ymlFilePath1 = "./src/test/resources/file1.yml";
     private final String ymlFilePath2 = "./src/test/resources/file2.yml";
+
+    private final String nestedYmlFilePath1 = "./src/test/resources/nestedFile1.yml";
+    private final String nestedYmlFilePath2 = "./src/test/resources/nestedFile2.yml";
     private final String incorrectFilePath = "./src/test/resources/file.kyml";
-    private final String expected = """
+    private final String simpleFileResultExpected = """
                 {
                   - follow: false
                     host: hexlet.io
@@ -22,17 +28,55 @@ public class DifferTest {
                   + timeout: 20
                   + verbose: true
                 }""";
+    private final String nestedFileResultExpected = """
+              {
+                  chars1: [a, b, c]
+                - chars2: [d, e, f]
+                + chars2: false
+                - checked: false
+                + checked: true
+                - default: null
+                + default: [value1, value2]
+                - id: 45
+                + id: null
+                - key1: value1
+                + key2: value2
+                  numbers1: [1, 2, 3, 4]
+                - numbers2: [2, 3, 4, 5]
+                + numbers2: [22, 33, 44, 55]
+                - numbers3: [3, 4, 5]
+                + numbers4: [4, 5, 6]
+                + obj1: {nestedKey=value, isNested=true}
+                - setting1: Some value
+                + setting1: Another value
+                - setting2: 200
+                + setting2: 300
+                - setting3: true
+                + setting3: none
+              }""";
+
+    @Disabled
+    public void nestedJsonDifferGenerateTest() throws Exception {
+        String actual = Differ.generate(nestedJsonFilePath1, nestedJsonFilePath2);
+        assertEquals(actual, nestedFileResultExpected);
+    }
+
+    @Disabled
+    public void nestedYmlDifferGenerateTest() throws Exception {
+        String actual = Differ.generate(nestedYmlFilePath1, nestedYmlFilePath2);
+        assertEquals(actual, nestedFileResultExpected);
+    }
 
     @Test
     public void jsonDifferGenerateTest() throws Exception {
         String actual = Differ.generate(jsonFilePath1, jsonFilePath2);
-        assertEquals(actual, expected);
+        assertEquals(actual, simpleFileResultExpected);
     }
 
     @Test
     public void ymlDifferGenerateTest() throws Exception {
         String actual = Differ.generate(ymlFilePath1, ymlFilePath2);
-        assertEquals(actual, expected);
+        assertEquals(actual, simpleFileResultExpected);
     }
 
     @Test
